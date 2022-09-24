93ec04c385a8d667b8bbd4c74d9f0af6b677978f
Armando Almanza <armando.s.almanza@gmail.com>
2022-09-24 01:07:03 -0500

    a

--- src/Components/Menu/Dish.jsx
+++ src/Components/Menu/Dish.jsx
@@ -1,11 +1,19 @@
-export const Dish = ({ title, imageUrl, description, color }) => {
+export const Dish = ({ title, imageUrl = "", description, color }) => {
     return (
         <div className={`card ${color}`}>
             <div className="card__title">
                 <h2>{title}</h2>
             </div>
             <div className={`card__content`}>
-                <img className="card__image" src={imageUrl} alt="Dish image" />
+                {imageUrl != "" ? (
+                    <img
+                        className="card__image"
+                        src={imageUrl}
+                        alt="Dish image"
+                    />
+                ) : (
+                    ""
+                )}
                 <p className="card__description">{description}</p>
             </div>
         </div>
